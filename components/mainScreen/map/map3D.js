91ddc6ccb1473603e280d5be970e13718210c518
Hanxiang Li <itsupport7@opulence.com>
2022-02-04 12:47:49 -0500

    map poly layer updated

--- components/mainScreen/map/map3D.js
+++ components/mainScreen/map/map3D.js
@@ -2,7 +2,7 @@ import {useRef, useEffect, useState, useCallback} from 'react';
 import countryCodeArr from '../../../src/countrycode-latlong.json';
 import {Register} from '../../../src/store/Register';
 import httpCalls from '../../commonFunction/httpCalls';
-// import hexDataset from "../../src/datasets/ne_110m_admin_0_countries.geojson";
+import hexDataset from "../../../src/ne_110m_admin_0_countries.json";
 
 const map3D = () => {
 
@@ -11,6 +11,7 @@ const map3D = () => {
   // const layoutFields = Register.useState((s) => s.layoutFields.financial);
   const store_res = Register.useState((s) => s.geo_info_res);
   const user_country = Register.useState((s) => s.geo_info_user_country);
+  const [countries, setCountries] = useState({ features: []});
   useEffect(() => {
     const fetchData = async () => {
       const api_result = await httpCalls(API_Route_Profile);
@@ -21,6 +22,7 @@ const map3D = () => {
         s.geo_info_user_country = api_result.data.address_details.country.toLowerCase();
       });
     };
+    setCountries(hexDataset);
     fetchData();
   }, [0]);
 
@@ -33,17 +35,47 @@ const map3D = () => {
     globeEl.current.controls().autoRotateSpeed = 0.1;
   }, []);
 
-
-  // useEffect(() => {
-  //   // load data
-  //   fetch('../datasets/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(setCountries);
-  // }, []);
-
-
   if (typeof window !== 'undefined') Globe = require('react-globe.gl').default;
 
   
   const globeEl = useRef();
+
+
+
+  let gData_target = Object.keys(countryCodeArr).filter(code => code == user_country);
+  const gData = gData_target.map((item, key) => ({
+    lat: countryCodeArr[item].lat,
+    lng: countryCodeArr[item].long,
+  }));
+
+  
+  return (
+    <Globe
+      ref={globeEl}
+      hexPolygonsData={countries.features}
+      hexPolygonColor={() => `#${Math.round(Math.random() * Math.pow(2, 24)).toString(16).padStart(6, '0')}`}
+      hexPolygonLabel={({ properties: d }) => `
+        <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+        Population: <i>${d.POP_EST}</i>
+      `}
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      // arcsData={arcsData}
+      pointsData={gData}
+      arcColor={'color'}
+      arcDashGap={2}
+      arcStroke={0.5}
+      arcDashAnimateTime={() => Math.random() * 8000 + 500}
+    />
+  );
+};
+
+export default map3D;
+
+
+
+
+
 
   // const [countries, setCountries] = useState({ features: [] });
   // const N = 20;
@@ -59,30 +91,3 @@ const map3D = () => {
   //   lat: countryCodeArr[item].lat,
   //   lng: countryCodeArr[item].long,
   // }));
-
-  let gData = Object.keys(countryCodeArr).filter(code => code == user_country);  
-  if(gData){
-    gData = [{
-      lat: countryCodeArr[gData[0]].lat,
-      lng: countryCodeArr[gData[0]].long,
-    }]
-  }
-
-  
-  
-  return (
-    <Globe
-      ref={globeEl}
-      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-      // arcsData={arcsData}
-      pointsData={gData}
-      arcColor={'color'}
-      arcDashGap={2}
-      arcStroke={0.5}
-      arcDashAnimateTime={() => Math.random() * 8000 + 500}
-    />
-  );
-};
-
-export default map3D;
